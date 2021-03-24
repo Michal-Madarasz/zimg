04016b3441c344f4c7e75e826f1eb2e5a13a153f
Michał Madarasz <238903@student.pwr.edu.pl>
2021-03-24 21:39:10 +0100

    Dodanie tworzenia rankingu cech
    - zamiana yes/no na 1/0 w tablicy
    - użycie algorytmu selectKBest do stworzenia rankingu
    - stworzenie diagramu z poszczególnymi cechami

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_selection import SelectKBest, f_classif
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     data = pd.read_csv("./data/prepared_data/diagnosis.csv", encoding='UTF-16')
-    print(data)
 
-    predictors = ["Temperature", "Nausea", "LumberPain", "ConUrine",
-                  "MictPains", "UrethraBurn"]
+    replace_data = ["Nausea", "LumberPain", "ConUrine", "MictPains", "UrethraBurn", "Inflammation", "Nephritis"]
+    predictors = ["Nausea", "LumberPain", "ConUrine", "MictPains", "UrethraBurn"]
 
-    selector = SelectKBest(f_classif, k=5)
-    # TODO How to handle text data in scikit-learn
-    # selector.fit(data[predictors], data["Inflammation"])
-    # scores = -np.log10(selector.pvalues_)
+    selector = SelectKBest(f_classif, k=3)
+    for i in predictors:
+        data[i] = data[i].map({'yes': 1, 'no': 0})
+
+    selector.fit_transform(data[predictors], data["Inflammation"])
+    scores = -np.log10(selector.pvalues_)
+    print(selector)
+    print(scores)
+
+    plt.bar(range(len(predictors)), scores)
+    plt.xticks(range(len(predictors)), predictors, rotation='horizontal')
+    plt.show()
