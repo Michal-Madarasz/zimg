58fa9e05020ee87c724fc0df717d3f086c1b7d26
Michał Madarasz <238903@student.pwr.edu.pl>
2021-04-21 21:10:49 +0200

    Uczenie modelu, liczenie dokładności klasyfikacji
    - różne miary odległości
    - różna liczba najbliższych sąsiadów
    - użyty algorytm kNN
    - wyjściowa średnia jakość w zależności od parametrów,
      klasyfikatorów

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import pandas as pd
 import numpy as np
-from sklearn.feature_selection import SelectKBest, f_classif
-import matplotlib.pyplot as plt
-from sklearn.model_selection import RepeatedKFold, train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import RepeatedKFold
+from sklearn.neighbors import KNeighborsClassifier
 
 from names_dict import COLUMNS
 
-if __name__ == '__main__':
-    data = pd.read_csv("./data/prepared_data/diagnosis.csv", encoding='UTF-16')
 
-    predictors = dict(COLUMNS[1:])
-
-    selector = SelectKBest(f_classif, k=3)
+def rebuild_table(tmp_data):
+    predictors = COLUMNS[1:]
     for i in predictors:
-        data[i] = data[i].map({'yes': 1, 'no': 0})
+        tmp_data[i] = tmp_data[i].map({'yes': 1, 'no': 0})
 
-    print(data)
 
+if __name__ == '__main__':
+    data = pd.read_csv('./data/prepared_data/diagnosis.csv', encoding='UTF-16')
     num_of_neighbours = [1, 5, 10]
-    type_of_metric = ["euclidean", "manhattan"]
+    type_of_metric = ['euclidean', 'manhattan']
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y,
-        test_size=.30,
-        random_state=1234
-    )
+    rebuild_table(data)
+    X = data.iloc[:, :-1].values
+    y = data.iloc[:, -1].values
 
     rkf = RepeatedKFold(n_splits=2, n_repeats=5, random_state=1234)
-    print(rkf)
-    scores = []
 
+    for metric in type_of_metric:
+        for neighbor in num_of_neighbours:
+            clf = KNeighborsClassifier(n_neighbors=neighbor, metric=metric)
+            scores = []
+            for train_index, test_index in rkf.split(X):
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+                clf.fit(X_train, y_train)
+                predict = clf.predict(X_test)
+                scores.append(accuracy_score(y_test, predict))
+
+            mean_score = np.mean(scores)
+            std_score = np.std(scores)
+            print(f"Miara odległości: {metric}")
+            print(f"Liczba najbliższych sąsiadów: {neighbor}")
+            print(f"Średnia jakość {mean_score:.3f}")
+            print(f"Odchylenie standardowe jakości {std_score:.3f}")
--- names_dict.py
+++ names_dict.py
@@ -1,9 +1,9 @@
-COLUMNS = (
-           ("Temperature", "Temperatura"),
-           ("Nausea", "występowanie nudności"),
-           ("LumberPain", "Ból lędźwiowy"),
-           ("ConUrine", "Parcie moczu (ciągła potrzeba oddawania moczu)"),
-           ("MictPains", "Bóle mikcji"),
-           ("UrethraBurn", "Pieczenie cewki moczowej, swędzenie, obrzęk ujścia cewki moczowej"),
-           ("Inflammation", "Zapalenie pęcherza moczowego"),
-)
+COLUMNS = [
+    "Temperature",
+    "Nausea",
+    "LumberPain",
+    "ConUrine",
+    "MictPains",
+    "UrethraBurn",
+    "Inflammation"
+]
